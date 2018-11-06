7b925a989bc42742feb173526e602800980e7994
Herb Stahl <ghstahl@gmail.com>
2018-11-06 13:33:20 -0800

    initial

---
+++ src/test/Person.cpp
@@ -0,0 +1,35 @@
+//
+// Created by ghsta on 11/6/2018.
+//
+
+#include "Person.h"
+#include <sstream>
+using namespace std;
+Person::Person() {}
+Person::Person(string firstName,string lastName) {
+    setFirstName(firstName)
+}
+Person::~Person() {
+
+}
+string Person::getFullName()  {
+    std::stringstream ss;
+    ss << FirstName << " " << LastName;
+    string fullName = ss.str();
+    return fullName;
+}
+string Person::getFirstName()  {
+    return FirstName;
+}
+
+void Person::setFirstName( string firstName) {
+    FirstName = firstName;
+}
+
+string Person::getLastName() {
+    return LastName;
+}
+
+void Person::setLastName( string lastName) {
+    LastName = lastName;
+}
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include "Person.h"
+
+using namespace std;
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    vector<Person> persons;
+    for(int i = 0; i<10;++i){
+        Person person;
+        std::stringstream ssFirstName;
+        ssFirstName << "First" << i;
+        person.setFirstName(ssFirstName.str());
+        std::stringstream ssLastName;
+        ssLastName << "Last" << i;
+        person.setLastName(ssLastName.str());
+        persons.push_back(person);
+    }
+
+    for(int i = 0; i<persons.size();++i){
+        string fullName = persons[i].getFullName();
+        std::cout << "Hello, " <<persons[i].getFullName()<< std::endl;
+    }
+
     return 0;
 }
